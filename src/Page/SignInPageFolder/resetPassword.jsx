1bfd7328e9236483e3bdb858f155f32244f994c7
KBandipo~ <keneebandipo@gmail.com>
2023-12-12 16:03:54 +0100

    Finish form section

--- src/Page/SignInPageFolder/resetPassword.jsx
+++ src/Page/SignInPageFolder/resetPassword.jsx
@@ -1,8 +1,8 @@
 function ResetPassword() {
   return (
     <>
-      <div>
-        <div className="grid w-[31%] grid-cols-[auto_auto] overflow-hidden">
+      <div className="box-border flex">
+        <div className=" w-[31%] overflow-hidden">
           <div className="h-screen w-full bg-[url('/image/passwordRecoveryImage.svg')] bg-cover bg-center bg-no-repeat">
             <div className="w-[90%] pl-[32px] pt-[38px] text-white">
               <img
@@ -18,7 +18,44 @@ function ResetPassword() {
           </div>
         </div>
         <div className="w-[69%]">
-          <h1>Jesus is Lord</h1>
+          <div className="mx-auto w-[48%] pt-[248px]">
+            <h2 className="text-center text-[49px] font-bold leading-[64px] text-[#444]">
+              Reset Password
+            </h2>
+            <h6 className="text-center text-[20px] font-normal leading-6 text-[#043133]">
+              We know how hard it is to remember password sometimes let’s help
+              you reset your password.
+            </h6>
+
+            <form className="mx-auto mt-[75px] w-[79%]">
+              <div className="mb-[49px] w-full text-[#444]">
+                <label className="block text-[18px] font-medium leading-6">
+                  Email address
+                </label>
+                <input
+                  type="email"
+                  className="mt-[3px] h-[60px] w-full rounded border-[0.6px] border-solid border-[#9B9B9B66] bg-[#E4E8EBBF] py-[18px] pl-[17px] text-[16px] font-normal leading-6"
+                  placeholder="Enter a valid email address"
+                />
+              </div>
+              <div className="text-center">
+                <button className="rounded bg-[#00666A] px-[40px] py-[16px] text-[18px] font-medium leading-6 text-white ">
+                  Reset Password
+                </button>
+              </div>
+              <div className="mt-[34px] text-center ">
+                <p className="text-[18px] font-normal leading-6">
+                  Don’t have an account?
+                  <a
+                    id="sign-up"
+                    className="text-[18px] font-medium leading-6 text-[#007074]"
+                  >
+                    Sign In
+                  </a>
+                </p>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
     </>
