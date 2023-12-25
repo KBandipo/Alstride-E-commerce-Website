482a46cffed7d3fe5d06fd0400cbe05b1b0c96ee
KBandipo~ <keneebandipo@gmail.com>
2023-12-25 12:29:48 +0100

    Creates files and folders for homePage and delete unused file (#14)

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,15 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import ResetPassword from "./Page/resetPassword/ResetPassword";
 
 import UpdatePassword from "./Page/updatePasswordPage/updatePassword";
+import LandingPage from "./Page/homePageFolder/LandingPage";
 
 function App() {
   return (
     <>
       <Router>
         <Routes>
-          {/* <Route path="/" element={<SignIn />} />
-          <Route path="/sign-up" element={<SignUp />} /> */}
-          <Route path="/" element={<ResetPassword />} />
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/reset-password" element={<ResetPassword />} />
           <Route path="/update-password" element={<UpdatePassword />} />
         </Routes>
       </Router>
---
+++ src/Page/homePageFolder/LandingPage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import HeroSection from "./HeroSection";
+
+function LandingPage() {
+  return (
+    <>
+      <div className="w-90% mx-auto">
+        <HeroSection />
+      </div>
+    </>
+  );
+}
+
+export default LandingPage;
