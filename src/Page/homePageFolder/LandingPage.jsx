fea10743588cc173d9179fac9a5c49ebe50e3d46
KBandipo~ <keneebandipo@gmail.com>
2023-12-26 11:20:53 +0100

    Make the HeroSection 90% of the landing page (#14)

--- src/Page/homePageFolder/LandingPage.jsx
+++ src/Page/homePageFolder/LandingPage.jsx
@@ -4,7 +4,7 @@ import HeroSection from "./HeroSection";
 function LandingPage() {
   return (
     <>
-      <div className="w-90% mx-auto">
+      <div className="mx-auto w-[90%]">
         <HeroSection />
       </div>
     </>
