4c0761c934cfd6abd045df3cc38a206bf9b7421b
KBandipo~ <keneebandipo@gmail.com>
2023-12-28 10:29:52 +0100

    Add (#14)

--- src/Page/homePageFolder/LandingPage.jsx
+++ src/Page/homePageFolder/LandingPage.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import HeroSection from "./HeroSection";
+import Header from "./Header";
 
 function LandingPage() {
   return (
     <>
+      <Header />
       <div className="mx-auto w-[90%]">
         <HeroSection />
       </div>
