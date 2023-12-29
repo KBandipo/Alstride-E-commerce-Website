792a147795056e19b8c1957d9b66a2f83431dbdc
KBandipo~ <keneebandipo@gmail.com>
2023-12-29 13:55:42 +0100

    reeshipping and receive the props (#14)

--- src/Page/homePageFolder/FreeShipping.jsx
+++ src/Page/homePageFolder/FreeShipping.jsx
@@ -1,9 +1,29 @@
 import React from "react";
+import FreeShippingComponent from "./HomeComponents/FreeShippingComponent";
 
 function FreeShipping() {
   return (
     <>
-      <div></div>
+      <div className="mb-[33px] mt-[24px] grid gap-[10px] sm:grid-cols-[1fr_1fr_1fr]">
+        <FreeShippingComponent
+          imageSource="public/homePageImages/delivery-concept-portrait-happy-african-american-delivery-man-red-cloth-holding-box-package-isolated-grey-studio-background-copy-space.svg"
+          imageAlt="delivery-concept-portrait-happy-african-american-delivery-man-red-cloth-holding-box-package"
+          textHeading="FREE SHIPPING"
+          textdescription="Free shipping on all orders"
+        />
+        <FreeShippingComponent
+          imageSource="public/homePageImages/person-office-during-work-day.svg"
+          imageAlt="person-office-during-work-day"
+          textHeading="ONLINE SUPPORT"
+          textdescription="24/7 customer support"
+        />
+        <FreeShippingComponent
+          imageSource="public/homePageImages/hands-putting-banknotes-envelope.svg"
+          imageAlt="hands-putting-banknotes-envelope"
+          textHeading="MONEY RETURN"
+          textdescription="Back guarantee under 5 days"
+        />
+      </div>
       <div></div>
     </>
   );
