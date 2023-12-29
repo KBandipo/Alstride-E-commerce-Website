96f5b802ea13d57c2819942dedc6b7cc96d649f5
KBandipo~ <keneebandipo@gmail.com>
2023-12-29 02:41:13 +0100

    Add footer section to LandingPage.jsx (#14)

--- src/Page/homePageFolder/LandingPage.jsx
+++ src/Page/homePageFolder/LandingPage.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import HeroSection from "./HeroSection";
 import Header from "./Header";
+import Footer from "./Footer";
 
 function LandingPage() {
   return (
@@ -9,6 +10,7 @@ function LandingPage() {
       <div className="mx-auto w-[90%]">
         <HeroSection />
       </div>
+      <Footer />
     </>
   );
 }
