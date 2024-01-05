91270ed4264d146b51d7b4b182f64f8110e06fd1
KBandipo~ <keneebandipo@gmail.com>
2024-01-06 00:46:31 +0100

    Create Checkout content's structure (#17)

--- src/Page/Checkout/Checkout.jsx
+++ src/Page/Checkout/Checkout.jsx
@@ -3,11 +3,26 @@ import React from "react";
 function Checkout() {
   return (
     <section>
-      {/* Upper Section: Delivery method, Voucher code and Shipping method  */}
-      <div></div>
+      {/* Upper Section: */}
+      <div>
+        {/*  Delivery method (Right)  */}
+        <div className="bg-#FEFDFA) w-[56%] shadow-[0_2px_20px_0px_rgba(0,0,0,0.15)] "></div>
+        {/* Left */}
+        <div className="w-[41%]">
+          {/* Voucher code */}
+          <div></div>
+          {/* Shipping method  */}
+          <div></div>
+        </div>
+      </div>
 
-      {/* Down Section: Order summary and Payment Method */}
-      <div></div>
+      {/* Down Section: */}
+      <div>
+        {/* Right Order summary*/}
+        <div className="w-[56%]"></div>
+        {/* Left Payment Method */}
+        <div className="w-[41%]"></div>
+      </div>
     </section>
   );
 }
