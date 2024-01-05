b01d66a88733f44f34c7bb823152d431dd4855c8
KBandipo~ <keneebandipo@gmail.com>
2024-01-05 13:46:27 +0100

    Add the Checkout contents (#17)

---
+++ src/Page/Checkout/Checkout.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Checkout() {
+  return (
+    <section>
+      {/* Upper Section: Delivery method, Voucher code and Shipping method  */}
+      <div></div>
+
+      {/* Down Section: Order summary and Payment Method */}
+      <div></div>
+    </section>
+  );
+}
+
+export default Checkout;
