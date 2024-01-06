ca7c215b538ebd2af5eb5bfbc8c46a0ed1b2ef2e
KBandipo~ <keneebandipo@gmail.com>
2024-01-06 01:20:40 +0100

    Modify code (#17)

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,20 @@
-import React from 'react';
+import React from "react";
 
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import ResetPassword from "./Page/resetPassword/ResetPassword";
 import UpdatePassword from "./Page/updatePasswordPage/updatePassword";
 import LandingPage from "./Page/homePageFolder/LandingPage";
-import ProductDetails from './Pages/ProductDetailsPage/ProductDetails';
-import ProductHome from './Pages/ProductDetailsPage/ProductHome';
-import Cart from './Pages/Cartfolder/Cart';
+import ProductDetails from "./Pages/ProductDetailsPage/ProductDetails";
+import ProductHome from "./Pages/ProductDetailsPage/ProductHome";
+import Cart from "./Pages/Cartfolder/Cart";
 // import Navigation from './Pages/Cartfolder/Navigation';
-import SignIn from './Page/SignInPagefolder/SignIn';
-import SignUp from './Page/SignupPageFolder/SignUp';
-import EmailVerification from './Page/SignupPageFolder/EmailVerification';
-
+import SignIn from "./Page/SignInPagefolder/SignIn";
+import SignUp from "./Page/SignupPageFolder/SignUp";
+import EmailVerification from "./Page/SignupPageFolder/EmailVerification";
+import ResetPassword from "./Page/resetPassword/ResetPassword";
 
 function App() {
   return (
     <>
-    
       <Router>
         <Routes>
           <Route path="/" element={<LandingPage />} />
@@ -25,7 +23,7 @@ function App() {
           <Route path="/email-verification" element={<EmailVerification />} />
           <Route path="/reset-password" element={<ResetPassword />} />
           <Route path="/update-password" element={<UpdatePassword />} />
-           <Route path="/product-home" element={<ProductHome />} />
+          <Route path="/product-home" element={<ProductHome />} />
           <Route path="/product/:id" element={<ProductDetails />} />
           <Route path="/cart" element={<Cart />} component={Cart} />
         </Routes>
