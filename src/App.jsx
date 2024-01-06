5cc07151604ea106a6e6b79cb3cd4d92f5fb6924
KBandipo~ <keneebandipo@gmail.com>
2024-01-06 01:54:10 +0100

    Modify code (#17)

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import UpdatePassword from "./Page/updatePasswordPage/UpdatePass";
+import UpdatePassword from "./Page/updatePasswordPage/UpdatePassword";
 import LandingPage from "./Page/homePageFolder/LandingPage";
 import ProductDetails from "./Pages/ProductDetailsPage/ProductDetails";
 import ProductHome from "./Pages/ProductDetailsPage/ProductHome";
