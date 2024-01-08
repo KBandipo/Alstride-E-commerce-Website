87f3020404203de7823533ec6bb9f19f94bc9f28
KBandipo~ <keneebandipo@gmail.com>
2024-01-08 20:32:28 +0100

    Create   src/Page/Checkout/CheckoutPages/ShippingMethod.jsx (#17)

---
+++ src/Page/Checkout/CheckoutPages/ShippingMethod.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function ShippingMethod() {
+  return (
+    <div className="bg-[#FEFDFA]  shadow-[0_2px_20px_0px_rgba(0,0,0,0.15)]"></div>
+  );
+}
+
+export default ShippingMethod;
