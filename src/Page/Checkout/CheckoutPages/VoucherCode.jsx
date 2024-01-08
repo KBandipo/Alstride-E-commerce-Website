ad9e508685db300a169950ed6354bfa0a206602a
KBandipo~ <keneebandipo@gmail.com>
2024-01-08 23:45:13 +0100

    Create contents of Voucher Code and Shipping Method (#17)

--- src/Page/Checkout/CheckoutPages/VoucherCode.jsx
+++ src/Page/Checkout/CheckoutPages/VoucherCode.jsx
@@ -1,8 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 function VoucherCode() {
+  const [voucherCode, setVoucherCode] = useState("");
+
+  const handleInputChange = (e) => {
+    setVoucherCode(e.target.value);
+  };
+
+  const handleApplyClick = () => {
+    // Here you can implement the logic to check the validity of the voucher code
+    // and apply any discounts or perform other actions accordingly.
+    console.log("Voucher code applied:", voucherCode);
+  };
+
   return (
-    <div className=" bg-[#FEFDFA] py-[50px] pl-3 shadow-[0_2px_20px_0px_rgba(0,0,0,0.15)]">
+    <div className="bg-[#FEFDFA] py-[50px] pl-3  shadow-[0_2px_20px_0px_rgba(0,0,0,0.15)]">
       {/* Voucher code */}
 
       <label
@@ -18,11 +30,13 @@ function VoucherCode() {
           id="voucherCode"
           className="rounded-[4px] border-[1px] border-solid border-[#686157] text-center text-[17px] font-normal leading-[24px] text-[#444444CC] "
           placeholder="Enter your voucher code"
-          value=""
+          value={voucherCode}
+          onChange={handleInputChange}
         />
 
         <button
           type="button"
+          onClick={handleApplyClick}
           className="rounded-[4px] border-[1px] border-solid border-[#007074] px-[24px] py-[8px] text-[14px] font-medium leading-[24px] text-[#696969] shadow-[0_2px_20px_0px_rgba(0,0,0,0.15)] hover:bg-[#007074] hover:text-white "
         >
           Apply
