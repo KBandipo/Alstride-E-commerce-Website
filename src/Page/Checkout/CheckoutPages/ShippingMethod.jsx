cec2acb92c53002ff75ade01833e90c023a95b13
KBandipo~ <keneebandipo@gmail.com>
2024-01-09 12:14:45 +0100

    Make the form of Shipping method responsive to different screens  (#17)

--- src/Page/Checkout/CheckoutPages/ShippingMethod.jsx
+++ src/Page/Checkout/CheckoutPages/ShippingMethod.jsx
@@ -16,34 +16,36 @@ function ShippingMethod() {
     console.log("Selected Options:", selectedOptions);
   };
 
+  const shippingOptions = [
+    {
+      option: "Standard shipping",
+      price: 25,
+      description: "Standard Flat-rate on all orders below $50",
+    },
+    {
+      option: "Expedited shipping",
+      price: 40,
+      description: "Expedited shipping to get item in a day or two",
+    },
+    {
+      option: "Overnight Shipping",
+      price: 77,
+      description: "Express shipping to get item on the next day",
+    },
+    {
+      option: "Free Shipping",
+      price: 0,
+      description: "On orders above $50",
+    },
+  ];
+
   return (
     <div className="w-full bg-[#FEFDFA] pb-[50px] shadow-[0_2px_20px_0px_rgba(0,0,0,0.15)]">
       <h3 className="mb-[30px] pt-[72px] text-center text-[31px] font-bold leading-[24px] ">
         Shipping Options
       </h3>
 
-      {[
-        {
-          option: "Standard shipping",
-          price: 25,
-          description: "Standard Flat-rate on all orders below $50",
-        },
-        {
-          option: "Expedited shipping",
-          price: 40,
-          description: "Expedited shipping to get item in a day or two",
-        },
-        {
-          option: "Overnight Shipping",
-          price: 77,
-          description: "Express shipping to get item on the next day",
-        },
-        {
-          option: "Free Shipping",
-          price: 0,
-          description: "On orders above $50",
-        },
-      ].map(({ option, price, description }, index) => (
+      {shippingOptions.map(({ option, price, description }, index) => (
         <div key={index}>
           <div className="flex w-full items-center justify-between pl-[24px]">
             <div className="flex items-center space-x-2">
@@ -64,7 +66,9 @@ function ShippingMethod() {
           <p className="pl-[48px] text-[16px] font-normal leading-[24px] text-[#A9A9A9] ">
             {description}
           </p>
-          <div className="h-[1px] w-full bg-[#444444B3] "></div>
+          {index !== shippingOptions.length - 1 && (
+            <div className="h-[1px] w-full bg-[#444444B3] "></div>
+          )}
         </div>
       ))}
     </div>
